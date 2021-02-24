73414636d17f19e53a782510e20e1363e50ee274
ddmeyer23 <78511312+ddmeyer23@users.noreply.github.com>
2021-02-24 02:12:13 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-require(["esri/config","esri/Map", "esri/views/MapView","esri/widgets/BasemapToggle", "esri/widgets/BasemapGallery", "esri/layers/FeatureLayer", "esri/WebMap",
-         "esri/core/urlUtils","esri/layers/CSVLayer","dojo/domReady!"], 
+require(["esri/config", "esri/Map", "esri/views/MapView", "esri/core/urlUtils","esri/layers/CSVLayer","dojo/domReady!"
+        ], 
       
-        function(esriConfig, Map, MapView, BasemapToggle, BasemapGallery, FeatureLayer, WebMap, urlUtils, CSVLayer){
+        function(esriConfig, Map, MapView, urlUtils, CSVLayer){
   
   //csv files require CORS enabled server if not on same domain as your website - so proxy required//
   var url = "https://raw.githubusercontent.com/gbrunner/Advanced_Python_for_GIS_and_RS/master/Week%202/stl_crime_wgs_84.csv";
